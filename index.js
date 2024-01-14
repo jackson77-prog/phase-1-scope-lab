b53d9d618e9f1c20fe2be2ae0c91756472666da7
Jackson <jackson.njihia@student.moringaschool.com>
2024-01-14 06:32:03 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  // Attempting to change a constant will result in an error because it cannot be changed.
+  
+}
+
+
+
